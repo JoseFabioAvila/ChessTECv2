94f76cd1a09b2392290298b0ac4d327ba3d266f6
sejolopab <gamboavargasjosepablo@gmail.com>
2016-05-07 15:37:22 -0600

    porSiAcaso

---
+++ ChessTEC/ParticionamientoTecnologico/Capa de Negocios/Arbol/A_Estrella.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessTEC.ParticionamientoTecnologico.Capa_de_Negocios.Arbol
+{
+    class A_Estrella
+    {
+    }
+}
