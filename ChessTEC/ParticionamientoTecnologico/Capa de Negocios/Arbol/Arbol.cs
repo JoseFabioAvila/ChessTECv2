2c68bda21928ec3fa293518ffd51e79758717c96
Jose Fabio Avila Estrada <fabioavila-16@hotmail.com>
2016-05-11 10:13:21 -0600

    Clase padre arbol (Falta implementar bien)

--- ChessTEC/ParticionamientoTecnologico/Capa de Negocios/Arbol/Arbol.cs
+++ ChessTEC/ParticionamientoTecnologico/Capa de Negocios/Arbol/Arbol.cs
@@ -8,5 +8,72 @@ namespace ChessTEC.ParticionamientoTecnologico.Capa_de_Negocios.Arbol
 {
     class Arbol
     {
+        private NodoArbol root;
+
+        /// <summary>Constructs the tree</summary>
+        /// <param name="value">the value of the node</param>
+        public Arbol(Tablero value)
+        {
+            if (value == null)
+            {
+                throw new ArgumentNullException(
+                    "Cannot insert null value!");
+            }
+
+            this.root = new NodoArbol(value);
+        }
+
+        /// <summary>Constructs the tree</summary>
+        /// <param name="value">the value of the root node</param>
+        /// <param name="children">the children of the root
+        /// node</param>
+        public Arbol(Tablero value, params NodoArbol[] children)
+        {
+            foreach (NodoArbol child in children)
+            {
+                this.root.AddChild(child);
+            }
+        }
+
+        /// <summary>
+        /// The root node or null if the tree is empty
+        /// </summary>
+        public NodoArbol Root
+        {
+            get
+            {
+                return this.root;
+            }
+        }
+
+        /// <summary>Traverses and prints tree in
+        /// Depth-First Search (DFS) manner</summary>
+        /// <param name="root">the root of the tree to be
+        /// traversed</param>
+        /// <param name="spaces">the spaces used for
+        /// representation of the parent-child relation</param>
+        private void PrintDFS(NodoArbol root, string spaces)
+        {
+            if (this.root == null)
+            {
+                return;
+            }
+
+            Console.WriteLine(spaces + root.Value);
+
+            NodoArbol child = null;
+            for (int i = 0; i < root.ChildrenCount; i++)
+            {
+                child = root.GetChild(i);
+                PrintDFS(child, spaces + "   ");
+            }
+        }
+
+        /// <summary>Traverses and prints the tree in
+        /// Depth-First Search (DFS) manner</summary>
+        public void TraverseDFS()
+        {
+            this.PrintDFS(this.root, string.Empty);
+        }
     }
 }
--- ChessTEC/ParticionamientoTecnologico/Capa de Negocios/Arbol/NodoArbol.cs
+++ ChessTEC/ParticionamientoTecnologico/Capa de Negocios/Arbol/NodoArbol.cs
@@ -8,5 +8,78 @@ namespace ChessTEC.ParticionamientoTecnologico.Capa_de_Negocios.Arbol
 {
     class NodoArbol
     {
+        // Contains the value of the node
+        private Tablero value;
+
+        // Shows whether the current node has a parent or not
+        private bool hasParent;
+
+        // Contains the children of the node (zero or more)
+        private List<NodoArbol> children;
+
+        /// <summary>Constructs a tree node</summary>
+        /// <param name="value">the value of the node</param>
+        public NodoArbol(Tablero value)
+        {
+            if (value == null)
+            {
+                throw new ArgumentNullException(
+                    "Cannot insert null value!");
+            }
+            this.value = value;
+            this.children = new List<NodoArbol>();
+        }
+
+        /// <summary>The value of the node</summary>
+        public Tablero Value
+        {
+            get
+            {
+                return this.value;
+            }
+            set
+            {
+                this.value = value;
+            }
+        }
+
+        /// <summary>The number of node's children</summary>
+        public int ChildrenCount
+        {
+            get
+            {
+                return this.children.Count;
+            }
+        }
+
+        /// <summary>Adds child to the node</summary>
+        /// <param name="child">the child to be added</param>
+        public void AddChild(NodoArbol child)
+        {
+            if (child == null)
+            {
+                throw new ArgumentNullException(
+                    "Cannot insert null value!");
+            }
+
+            if (child.hasParent)
+            {
+                throw new ArgumentException(
+                    "The node already has a parent!");
+            }
+
+            child.hasParent = true;
+            this.children.Add(child);
+        }
+
+        /// <summary>
+        /// Gets the child of the node at given index
+        /// </summary>
+        /// <param name="index">the index of the desired child</param>
+        /// <returns>the child on the given position</returns>
+        public NodoArbol GetChild(int index)
+        {
+            return this.children[index];
+        }
     }
 }
