086f689f8631f37e3a932ad3af80d966c9deab47
sejolopab <gamboavargasjosepablo@gmail.com>
2016-05-12 16:07:22 -0600

    commit

--- ChessTEC/ParticionamientoTecnologico/Capa Logica/Arbol/Recorrido.cs
+++ ChessTEC/ParticionamientoTecnologico/Capa Logica/Arbol/Recorrido.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ChessTEC.ParticionamientoTecnologico.Capa_de_Negocios;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,8 @@ namespace ChessTEC.ParticionamientoTecnologico.Capa_Logica.Arbol
 {
     class Recorrido
     {
+        public Tablero tablero { get; set; }
+        public List<Tablero> recorrido { get; set; }
+
     }
 }
