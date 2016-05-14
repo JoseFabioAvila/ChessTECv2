ecd8c30094296e66b3d1685a9f5b9069efe96abe
sejolopab <gamboavargasjosepablo@gmail.com>
2016-05-14 11:37:10 -0600

    gogo

--- ChessTEC/ParticionamientoTecnologico/Capa Logica/Arbol/Arbol2.cs
+++ ChessTEC/ParticionamientoTecnologico/Capa Logica/Arbol/Arbol2.cs
@@ -9,6 +9,13 @@ namespace ChessTEC.ParticionamientoTecnologico.Capa_Logica.Arbol
 {
     class Arbol2
     {
-        
+        public List<Nodo2> variables { get; set; }
+        public string turno { get; set; }
+        public Nodo2 mejorVariante { get; set; }
+
+        public Arbol2(Tablero t)
+        {
+            Nodo2 raiz = new Nodo2(t,"",t.turno);
+        }
     }
 }
